659a514c56ab6ce5927b99e3703115b4c51809ff
JoseLRiano <joselriano@yahoo.com>
2020-06-06 08:11:05 -0400

    Updates

---
+++ precache-manifest.559f340ec75d781a508a93a2a363c95d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "039aa95f23fb3803663e02613e8eb3ff",
+    "url": "/DogApp/index.html"
+  },
+  {
+    "revision": "230a2c363fe7d7174f4b",
+    "url": "/DogApp/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "7201124100ade7989742",
+    "url": "/DogApp/static/css/main.1d9d24a8.chunk.css"
+  },
+  {
+    "revision": "230a2c363fe7d7174f4b",
+    "url": "/DogApp/static/js/2.697c0c6b.chunk.js"
+  },
+  {
+    "revision": "aa2937bbd23b56ebb8238c918e07b5d0",
+    "url": "/DogApp/static/js/2.697c0c6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7201124100ade7989742",
+    "url": "/DogApp/static/js/main.676d7157.chunk.js"
+  },
+  {
+    "revision": "727cddfe624223c14cc3",
+    "url": "/DogApp/static/js/runtime-main.8d7aa13d.js"
+  },
+  {
+    "revision": "4eb85e80248f3371f721a9687e16d075",
+    "url": "/DogApp/static/media/hazel.4eb85e80.jpg"
+  },
+  {
+    "revision": "2f47cb659ae081c9e07bc25c264f0aed",
+    "url": "/DogApp/static/media/tubby.2f47cb65.jpg"
+  },
+  {
+    "revision": "5c1e3b17efc4e2197352567ea0e262c1",
+    "url": "/DogApp/static/media/whiskey.5c1e3b17.jpg"
+  }
+]);
